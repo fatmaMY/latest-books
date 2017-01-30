86e436896226565cc14565ca2b5bef27f3e86c00
fatma yousef <famyaltimimi@yahoo.com>
2017-01-30 15:53:08 +0000

    correct url in link variable, remove slash

--- lib/latest_books/cli.rb
+++ lib/latest_books/cli.rb
@@ -12,11 +12,10 @@ class LatestBooks::CLI
   def list_books
     puts "New Released Books:"
     puts "^^^^^^^^^^^^^^^^^^^^"
-   @books= LatestBooks::Books.today
-   @books.each.with_index(1) do |book, i|
+    @books = LatestBooks::Books.today
+    @books.each.with_index(1) do |book, i|
      puts "#{i}- #{book.name} - #{book.author_name} - #{book.price}"
      puts "________________________________________________________________________"
-     #puts "http://www.barnesandnoble.com/#{@books[0].url.join(" ")}"
    end
   end
   def menu
@@ -25,16 +24,11 @@ class LatestBooks::CLI
       puts "Enter the number of the book you wanted to know more about or type list to see new released books again or type exit to inter:"
       puts "_________________________________________________________________________"
       input = gets.strip.downcase
-
-      #if input.to_i > 0
-    #  the_book = @books[input.to_i-1]
-         #puts "#{the_book.name} - #{the_book.author_name} - #{the_book.price}--#{the_book.url} "
-      if input=="list"
+      if input == "list"
         list_books
       elsif  input.to_i > 0
-      # system(open("http://www.barnesandnoble.com/#{@books[input.to_i-1].url.join(" ")}"))
-      # Launchy.open("http://www.barnesandnoble.com/#{@books[input.to_i-1].url[0]}")
-        link = "http://www.barnesandnoble.com/#{@books[input.to_i-1].url[0]}"
+
+        link = "http://www.barnesandnoble.com#{@books[input.to_i-1].url[0]}"
         if RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
           system "start #{link}"
         elsif RbConfig::CONFIG['host_os'] =~ /darwin/
@@ -47,7 +41,7 @@ class LatestBooks::CLI
       end
     end
   end
-  
+
   def see_you
     puts "Thank you for using our application. See you next time!"
     puts "***     *** "
